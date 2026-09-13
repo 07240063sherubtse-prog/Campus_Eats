9049f41184d398ffdbd4aaa70a77d7d259dec704
Bobby <07240063.sherubtse@rub.edu.bt>
2026-09-13 23:22:10 +0600

    Unit 4: persistent storage and data models

--- app.js
+++ app.js
@@ -21,3 +21,9 @@ app.use('/', indexRoutes);
 app.listen(PORT, () => {
   console.log(`Campus Eats running at http://localhost:${PORT}`);
 });
+
+const db = require('./config/db');
+app.get('/db-test', async (req, res) => {
+  const result = await db.one('SELECT NOW() AS current_time');
+  res.json(result);
+});
